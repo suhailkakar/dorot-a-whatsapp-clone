8294d6cac00cb7ef722f1f3fbd6f04602c449103
Suhail Kakar <56798748+suhailkakar@users.noreply.github.com>
2021-05-10 20:24:58 +0530

    Update stories.js

---
+++ pages/stories.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class stories extends Component {
+    render() {
+        return (
+            <div>
+                <p>Stories</p>
+            </div>
+        )
+    }
+}
